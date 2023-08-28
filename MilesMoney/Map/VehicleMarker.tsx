b0c829214b355f29cebc87666bbc04e22ce1693f
Florian König <koenidv@gmail.com>
2023-08-29 00:40:41 +0200

    Custom markers

--- MilesMoney/Map/VehicleMarker.tsx
+++ MilesMoney/Map/VehicleMarker.tsx
@@ -1,9 +1,18 @@
 import {Vehicle} from "../lib/Miles/types";
 import * as React from "react";
+import { View } from "react-native";
 import Background from "../assets/icons/Marker/background.svg";
+import FullCharge from "../assets/icons/Marker/charge_30.svg";
+import VW_ID3 from "../assets/icons/Marker/VW_ID3.svg";
 
 const VehicleMarker = /*(vehicle: Vehicle)*/() => {
-  return <Background width={100} height={100} />;
+  return (
+    <View>
+      <Background style={{"position": "absolute"}} width={40} height={40} />
+      <FullCharge style={{"position": "absolute"}} width={40} height={40} />
+      <VW_ID3 style={{"position": "absolute"}} width={40} height={40} />
+    </View>
+  );
 };
 
 export default VehicleMarker;
