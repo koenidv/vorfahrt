5d4b9677f517bb052734003601911612daff4036
Florian König <koenidv@gmail.com>
2023-09-02 23:55:33 +0200

    Set up map for iOS

--- MilesMoney/ios/MilesMoney/AppDelegate.mm
+++ MilesMoney/ios/MilesMoney/AppDelegate.mm
@@ -1,12 +1,14 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"MilesMoney";
+  [GMSServices provideAPIKey:@"AIzaSyBkbwJdL1MFQizaglC4dvsLK3FpZGBiRXo"];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
