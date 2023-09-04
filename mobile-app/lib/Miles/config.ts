fd8f84bc1a51938672a72c6e499ff90f733f64a4
Florian König <koenidv@gmail.com>
2023-09-05 00:17:25 +0200

    New device key

--- mobile-app/lib/Miles/config.ts
+++ mobile-app/lib/Miles/config.ts
@@ -1,2 +1,3 @@
 export const BASE_URL = "https://api.app.miles-mobility.com/mobile";
-export const DEVICE_KEY = "d15231c7925b4517";
+export const DEVICE_KEY = "d15231c7925b4520";
+// todo implement device key generation
