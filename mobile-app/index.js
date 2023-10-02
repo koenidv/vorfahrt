63ed583b67b3c0f0cefe22e0705aab1c199c4cff
Florian König <koenidv@gmail.com>
2023-10-02 11:00:47 +0200

    chore(mobile): add react native paper watch out: icons are not installed for ios currently https://github.com/oblador/react-native-vector-icons#ios

--- mobile-app/index.js
+++ mobile-app/index.js
@@ -1,5 +1,14 @@
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { PaperProvider, MD3DarkTheme } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+function Main() {
+    return (
+        <PaperProvider theme={MD3DarkTheme}>
+            <App />
+        </PaperProvider>
+    )
+}
+
+AppRegistry.registerComponent(appName, () => Main);
