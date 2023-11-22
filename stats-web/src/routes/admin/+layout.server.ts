d5bc8671b3c3f5722262f6c08eed2010494188cc
Florian König <koenidv@gmail.com>
2023-11-22 20:43:34 +0100

    chore(stats): basic error handling

--- stats-web/src/routes/admin/+layout.server.ts
+++ stats-web/src/routes/admin/+layout.server.ts
@@ -1,7 +1,30 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "../$types";
+import { PUBLIC_ADMIN_URL } from "$env/static/public";
 
 export const load: LayoutServerLoad = async (event) => {
   const session = await event.locals.getSession();
   if (!session?.user) throw redirect(303, "/login?redirect=" + event.route.id);
+
+  // fixme not reaching the admin api will result in 500
+  try {
+    // todo trpc
+    const session = await event.locals.getSession();
+    const scrapersRes = await fetch(PUBLIC_ADMIN_URL + "/scraper", {
+      headers: {
+        "Authorization": "Bearer " + session!.accessToken
+      }
+    })
+    const scrapers = await scrapersRes.json();
+    return { scrapers };
+  } catch (e) {
+    console.error(e);
+    return {
+      scrapers: [],
+      error: {
+        message: "Admin API not reachable",
+        error: e?.toString?.() || JSON.stringify(e)
+      }
+    }
+  }
 };
--- stats-web/src/routes/admin/+layout.svelte
+++ stats-web/src/routes/admin/+layout.svelte
@@ -13,24 +13,33 @@
   </div>
 </div>
 
-<div class="m-4">
-  <div class="flex flex-row w-100 max-w gap-3 min-h-[80vh] overflow-hidden">
-    <ul class="menu bg-base-200 rounded-box w-56 pb-3 h-fit flex-shrink-0">
-      <li><a href="/admin" class={$page?.route?.id === "/admin" ? "active": ""}>Overview</a></li>
-      <div class="divider my-2">Scrapers</div>
-      {#each $page.data.scrapers as scraper}
-        <li>
-          <a
-            href={"/admin/scraper/" + scraper.name}
-            class={$page?.params?.scraperId === scraper.name ? "active" : ""}>
-            {scraper.name}</a>
-        </li>
-      {/each}
-      <div class="divider my-2">Influx Clients</div>
-      to be developed
-    </ul>
-    <div class="flex-grow bg-base-200 rounded-box p-4 overflow-hidden">
-      <slot />
+{#if !$page.data.error}
+  <div class="m-4">
+    <div class="flex flex-row w-100 max-w gap-3 min-h-[80vh] overflow-hidden">
+      <ul class="menu bg-base-200 rounded-box w-56 pb-3 h-fit flex-shrink-0">
+        <li><a href="/admin" class={$page?.route?.id === "/admin" ? "active" : ""}>Overview</a></li>
+        <div class="divider my-2">Scrapers</div>
+        {#each $page.data.scrapers as scraper}
+          <li>
+            <a
+              href={"/admin/scraper/" + scraper.name}
+              class={$page?.params?.scraperId === scraper.name ? "active" : ""}>
+              {scraper.name}</a>
+          </li>
+        {/each}
+        <div class="divider my-2">Influx Clients</div>
+        to be developed
+      </ul>
+      <div class="flex-grow bg-base-200 rounded-box p-4 overflow-hidden">
+        <slot />
+      </div>
     </div>
   </div>
-</div>
+{:else}
+  <div class="flex w-full justify-center">
+    <div class="flex flex-col jusify-center items-center rounded-box bg-error px-8 py-4 mt-12">
+      <h2 class="text-error-content text-md">{$page.data.error.message}</h2>
+      <p class="text-error-content">{$page.data.error.error}</p>
+    </div>
+  </div>
+{/if}
