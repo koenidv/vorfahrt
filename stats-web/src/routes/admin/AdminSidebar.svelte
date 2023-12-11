367848aa8e0f43287b53c25f647e6839fd82484e
Florian König <koenidv@gmail.com>
2023-12-11 13:36:44 +0100

    feat(stats): search by license plate

--- stats-web/src/routes/admin/AdminSidebar.svelte
+++ stats-web/src/routes/admin/AdminSidebar.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import { page } from "$app/stores";
   import "iconify-icon";
 
   export let services: { [key: string]: Service };
+
+  let licensePlateValue = /.*\/vehicle\/([^\/]*)/.exec($page?.route?.id || "")?.[1] ?? "";
+  function onLicensePlateInput(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      goto("/admin/vehicle/" + licensePlateValue);
+    }
+  }
 </script>
 
 <ul class="menu bg-base-200 rounded-box w-56 pb-3 h-fit flex-shrink-0">
   <li><a href="/admin" class={$page?.route?.id === "/admin" ? "active" : ""}>Overview</a></li>
+  <li>
+    <input
+      type="text"
+      placeholder="Find Vehicle"
+      class="input input-bordered input-md w-full max-w-xs mt-2 mb-1"
+      bind:value={licensePlateValue}
+      on:keydown={onLicensePlateInput} />
+  </li>
   <div class="divider my-2">Scrapers</div>
   {#each Object.entries(services).filter((s) => s[1].type === "scraper") as [id, scraper]}
     <li>
